e29e49d7db53ca170ba233ee6a2b433b4e410273
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-24 10:08:23 +0300

    Some changes in Models

--- movies/models.py
+++ movies/models.py
@@ -4,11 +4,18 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    genre = models.CharField(max_length=255)
     release = models.DateField()
+    genre = models.ForeignKey('movies.Genre', on_delete=models.CASCADE)
     poster = models.ImageField(
         upload_to='cookie/images/movies/', null=False
     )
+
+    def __str__(self):
+        return self.title
+
+
+class Genre(models.Model):
+    title = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title
