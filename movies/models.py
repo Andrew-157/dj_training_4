4bae812f684e678689f893dc72be5a8ecc844d75
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-24 09:35:38 +0300

    Created model Movie

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    genre = models.CharField(max_length=255)
+    release = models.DateField()
+    poster = models.ImageField(
+        upload_to='cookie/images/movies/', null=False
+    )
+
+    def __str__(self):
+        return self.title
