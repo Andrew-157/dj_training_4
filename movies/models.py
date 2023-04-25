4115f827c5ac18653b6a980ef2b491e2dba2a4a5
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-25 13:17:27 +0300

    Created model Review

--- movies/models.py
+++ movies/models.py
@@ -32,3 +32,10 @@ class Rating(models.Model):
     rating = models.PositiveSmallIntegerField(choices=rating_choices)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     rated_by = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+    review_content = models.TextField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    reviewed_by = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now=True)
