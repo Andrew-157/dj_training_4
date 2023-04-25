d56adcbcbef3c81e2c2c17e99d4401659e7122fd
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-25 10:23:55 +0300

    MovieDetailView

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from movies import views
 
+
+app_name = 'movies'
 urlpatterns = [
-    path('movies/<str:genre>/', views.MovieListView.as_view(), name='movie-list')
+    path('movies/genres/<str:genre>/',
+         views.MovieListView.as_view(), name='movie-list'),
+    path('movies/<int:pk>/', views.MovieDetailView.as_view(), name='movie-detail')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.detail import SingleObjectMixin
+from django.views import View
 from taggit.models import Tag
 from movies.models import Movie
 
@@ -13,10 +15,19 @@ class MovieListView(ListView):
         genre = self.kwargs['genre']
         genre_object = Tag.objects.filter(slug=genre).first()
         movies = Movie.objects.prefetch_related(
-            'genres').filter(genres=genre_object).all()
+            'genres').filter(genres=genre_object).order_by('-release_date').all()
         return movies
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['genre'] = ' '.join(self.kwargs['genre'].split('-'))
         return context
+
+
+class MovieDetailView(View):
+    def get(self, request, *args, **kwargs):
+        movie = Movie.objects.prefetch_related(
+            'genres').filter(pk=kwargs['pk']).first()
+        if not movie:
+            return render(request, 'movies/nonexistent.html')
+        return render(request, 'movies/movie_detail.html', {'movie': movie})
