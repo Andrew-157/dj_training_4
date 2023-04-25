fe94f6baaeb05477258322ed114afd775cf8774c
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-25 13:19:01 +0300

    Created ReviewMovieForm

--- movies/forms.py
+++ movies/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from movies.models import Rating
+from movies.models import Rating, Review
 
 
 class RateMovieForm(forms.ModelForm):
     class Meta:
         model = Rating
         fields = ['rating']
+
+
+class ReviewMovieForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['review_content']
