bd886dba37ce66f9bac5cb007f492f5d00d0f472
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-28 10:11:28 +0300

    Some changes

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,14 @@
+from django.views.generic import TemplateView
 from django.urls import path
 from movies import views
 
 
 app_name = 'movies'
 urlpatterns = [
+    path('', TemplateView.as_view(
+        template_name='movies/index.html'), name='index'),
+    path('become_user/', TemplateView.
+         as_view(template_name='movies/become_user.html'), name='become-user'),
     path('movies/genres/<str:genre>/',
          views.MoviesByGenre.as_view(), name='movies-by-genre'),
     path('movies/<int:pk>/', views.MovieDetailView.as_view(), name='movie-detail'),
--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm,\
     UserChangeForm as BaseUserChangeForm
 
@@ -7,6 +8,7 @@ class UserCreationForm(BaseUserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
+        model = User
         fields = [
             'username', 'email', 'password1', 'password2'
         ]
@@ -14,8 +16,10 @@ class UserCreationForm(BaseUserCreationForm):
 
 class UserChangeForm(BaseUserChangeForm):
     email = forms.EmailField(required=True)
+    password = None
 
     class Meta:
+        model = User
         fields = [
             'username', 'email'
         ]
