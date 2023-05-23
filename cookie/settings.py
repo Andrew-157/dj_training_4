a5b79bf811e5623640901ff85fde057a5c5b9b89
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-23 12:31:56 +0300

    Migrated to PostgreSQL

--- cookie/settings.py
+++ cookie/settings.py
@@ -14,6 +14,10 @@ import os
 from pathlib import Path
 from django.urls import reverse_lazy
 from django.contrib import messages
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,8 +91,12 @@ WSGI_APPLICATION = 'cookie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT")
     }
 }
 
