89bc49f41bbf81a31150126d4989c11558a89f48
Andrew Subotin <subotinandrey5@gmail.com>
2023-05-23 14:28:19 +0300

    validate poster size

--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,19 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from taggit.managers import TaggableManager
+
+
+def validate_image(image):
+    file_size = image.file.size
+    limit_kb = 150
+    if file_size > limit_kb * 1024:
+        raise ValidationError(f"Maximum size of the image is {limit_kb} KB")
+
+    # file_size = image.file.size
+    # limit_mb = 8
+    # if file_size > limit_mb * 1024 * 1024:
+    #     raise ValidationError(f"Maximum size of the image is {limit_mb} MB")
 
 
 class Movie(models.Model):
@@ -20,7 +33,8 @@ class Movie(models.Model):
     country = models.CharField(max_length=2, choices=COUNTRIES)
     genres = TaggableManager(verbose_name='genres',
                              help_text='A comma-separated list of genres.')
-    poster = models.ImageField(upload_to='cookie/images/movies/', null=False)
+    poster = models.ImageField(
+        upload_to='cookie/images/movies/', null=False, validators=[validate_image])
 
     def __str__(self):
         return self.title
